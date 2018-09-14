490ac410652f2387f7b811de575cb7242faf2dd0
mohammad-gitup <mohammadsyed.kc@gmail.com>
2018-09-14 16:01:44 -0500

    ADD: whats good

--- server.js
+++ server.js
@@ -2,17 +2,11 @@ const express = require('express');
 const app = express()
 
 app.get('/', function(req, res) {
-  res.send('Hello World!')
+  res.snd('Hello World!')
 })
 
 
 
 app.listen('3000', function() {
   console.log('Server listening on port 3000!');
-})
-
-
-// mehul added line
-app.listen('3001', function() {
-  console.log('Server listening on port 3001!');
 })
