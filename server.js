b58bb7942cd4a07230f780053548d285d3d66ad6
mohammad-gitup <mohammadsyed.kc@gmail.com>
2018-09-18 11:28:47 -0500

    ADD: setting up HBS

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const path = require('path');
 const app = express()
 
+
+const hbs = require('express-handlebars')({
+  defaultLayout: 'main',
+  extname: '.hbs'
+});
+
+app.engine('hbs', hbs);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+
 app.get('/', function(req, res) {
-  res.snd('Hello World!')
+  res.render('home')
 })
 
+app.get('/student', function(req, res) {
+  // opens this homepage
+  // retrieves from database
+})
 
 
 app.listen('3000', function() {
